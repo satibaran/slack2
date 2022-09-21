1b31eba9ec9550dee93402a7580592552f442d85
baranmmm <m.baran84@gmail.com>
2022-09-21 09:58:43 +0100

    add Login.java

---
+++ src/day_1/CreateChannel.java
@@ -0,0 +1,15 @@
+package day_1;
+
+public class CreateChannel {
+    public static void main(String[] args) {
+
+        String s = "string";
+        if (s.contains("string")) {
+
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+
+        }
+    }
+}
---
+++ src/day_1/Login.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class Login {
+    public static void main(String[] args) {
+        //The user can login succesfully...
+    }
+}
